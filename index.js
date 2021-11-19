d66c4e01f23083fb59903bad0d4981d07fd58a38
Psyche47 <emmanuel.gomes7181@gmail.com>
2021-11-19 19:44:22 +0600

    Scripts added

---
+++ index.js
@@ -0,0 +1,107 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.8ub5n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("shutter");
+    const productCollection = database.collection("allProducts");
+    const ordersCollection = database.collection("bookings");
+
+    // GET API
+    app.get("/allProducts", async (req, res) => {
+      const cursor = productCollection.find({});
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    // POST API
+    app.post("/addProducts", async (req, res) => {
+      const service = req.body;
+      const result = await productCollection.insertOne(service);
+      res.json(result);
+    });
+
+    // SINGLE PRODUCT
+    app.get("/singleProduct/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await productCollection.findOne(query);
+      res.json(service);
+    });
+
+    // CONFIRM BOOKING
+    app.post("/confirmedOrders", async (req, res) => {
+      const booking = req.body;
+      const result = await ordersCollection.insertOne(booking);
+      res.json(result);
+    });
+
+    // MY BOOKINGS
+    app.get("/myOrders/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await ordersCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // GET ALL BOOKINGS
+    app.get("/myOrders", async (req, res) => {
+      const cursor = ordersCollection.find({});
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // DELETE BOOKINGS
+    app.delete("/deleteOrder/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await ordersCollection.deleteOne(query);
+      res.json(result);
+    });
+
+    // DELETE SERVICE
+    app.delete("/deleteProduct/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.json(result);
+    });
+
+    // UPDATE STATUS
+    app.put("/updateOrderStatus/:id", (req, res) => {
+      const id = req.params.id;
+      const updatedStatus = req.body.status;
+      const filter = { _id: ObjectId(id) };
+      ordersCollection
+        .updateOne(filter, { $set: { status: updatedStatus } })
+        .then((result) => res.send(result));
+    });
+  } finally {
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Shutter server is running.");
+});
+
+app.listen(port, () => {
+  console.log("Shutter server is running on port: ", port);
+});
